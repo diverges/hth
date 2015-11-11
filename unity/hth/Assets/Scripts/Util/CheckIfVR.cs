27a9920585ffcc29b1ca46850212768b8cdced06
unknown <sotolongomiguel@gmail.com>
2015-11-11 17:42:39 -0500

    VR stuff

---
+++ unity/hth/Assets/Scripts/Util/CheckIfVR.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class CheckIfVR : MonoBehaviour {
+
+    public GameObject vr_Cam;
+    public GameObject p1_Cam;
+
+	// Use this for initialization
+	void Start () {
+        if (VRSettings.loadedDevice != VRDeviceType.None)
+        {
+            Debug.Log("VR Device Detected");
+            p1_Cam.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No VR Device Detected");
+            vr_Cam.SetActive(false);
+        }
+	}
+
+}
